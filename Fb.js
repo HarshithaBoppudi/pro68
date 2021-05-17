dbb161dbd0639eee485640f3ac7231ce5a837883
HarshithaBoppudi <67325628+HarshithaBoppudi@users.noreply.github.com>
2021-05-17 20:41:40 +0530

    Add files via upload

---
+++ Fb.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, StyleSheet, Button,TextInput,Text,TouchableOpacity,Image } from 'react-native';
+
+
+export default class Fb extends React.Component {
+render(){
+  return(
+    <View>
+    <Text style={{marginTop:100}}>FaceBook</Text>
+    </View>
+  )
+}
+
+}
---
+++ In.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, StyleSheet, Button,TextInput,Text,TouchableOpacity,Image } from 'react-native';
+
+
+export default class In extends React.Component {
+render(){
+  return(
+    <View>
+    <Text style={{marginTop:100}}>Instgram</Text>
+    </View>
+  )
+}
+
+}
